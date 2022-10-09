c015c4ba0357dab3ba3897b56464fdeea5b432b8
Terry Hsiesh <lin020488995@gmail.com>
2022-10-10 01:17:23 +0800

    fix 訂單資訊無法回覆

--- api/v1/line/camp.go
+++ api/v1/line/camp.go
@@ -347,15 +347,18 @@ func reply_date_limit(bot *linebot.Client, event *linebot.Event) {
 }
 
 func parase_Order_Info(info string) (bool, string) {
-
+	fmt.Println("收到訂單資訊", info)
 	var tmp Order_Info
 	split := strings.Split(info, "\n")
 	info_map := make(map[string]string)
 	for _, r := range split {
 		arr := strings.Split(r, ":")
-		if arr[0] != "訂單資訊" && arr[0] != "-----------" {
+		if strings.TrimSpace(arr[0]) != "訂單資訊" && arr[0] != "-----------" {
 			fmt.Println(arr)
-			info_map[arr[0]] = arr[1]
+			if arr[1] != "" {
+				info_map[arr[0]] = arr[1]
+			}
+
 		}
 	}
 	fmt.Println(info_map)
